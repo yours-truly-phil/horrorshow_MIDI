582d106f72a3cefb3ebe8ff34b4f25b7863b0e0d
Philipp Seeger <real@horrorshow.io>
2020-03-18 18:47:05 +0100

    NEGARM-1 Googletest Integration removage

--- horrorshow_MIDI/Source/MidiProcessorTest.cpp
+++ horrorshow_MIDI/Source/MidiProcessorTest.cpp
@@ -8,4 +8,28 @@
   ==============================================================================
 */
 
+//#include "MidiProcessor.h"
 #include "gtest/gtest.h"
+
+//class MidiProcessorTest : public ::testing::Test
+//{
+//protected:
+//  MidiProcessorTest();
+//  ~MidiProcessorTest() override;
+//
+//  void SetUp() override;
+//  void TearDown() override;
+//};
+//
+//TEST_F(MidiProcessorTest, GetNegHarmNnValidMidiNn)
+//{
+//  // [24;127] are valid Midi NoteNumbers
+//  // TODO
+//  EXPECT_GE(24, 24);
+//}
+//
+//int main(int argc, char **argv)
+//{
+//  ::testing::InitGoogleTest(&argc, argv);
+//  return RUN_ALL_TESTS();
+//}
