a3c3b6a3e9e783ac5a19d3284098c04481fcbadb
Philipp Seeger <philippbseeger@googlemail.com>
2020-03-24 15:08:30 +0100

    NEGARM-4 using .clang-format based on LLVM and JUCE Guidelines inspired by https://forum.juce.com/t/automatic-juce-like-code-formatting-with-clang-format/31624/13

--- horrorshow_MIDI/Source/PluginProcessor.h
+++ horrorshow_MIDI/Source/PluginProcessor.h
@@ -8,7 +8,6 @@
   ==============================================================================
 */
 
-
 #pragma once
 
 #include "../JuceLibraryCode/JuceHeader.h"
@@ -35,52 +34,72 @@ constexpr auto kIdMidiNNoPianoMax = "id_max_piano_midi_note_number";
 //==============================================================================
 /**
 */
-class ToNegativeHarmonyProcessor : public AudioProcessor {
- public:
-  //==============================================================================
-  ToNegativeHarmonyProcessor();
-  ~ToNegativeHarmonyProcessor() override;
+class ToNegativeHarmonyProcessor : public AudioProcessor
+{
+public:
+    //==============================================================================
+    ToNegativeHarmonyProcessor();
+    ~ToNegativeHarmonyProcessor() override;
 
-  //==============================================================================
-  void prepareToPlay(double sample_rate, int samples_per_block) override;
-  void releaseResources() override;
+    //==============================================================================
+    void
+    prepareToPlay(double sample_rate, int samples_per_block) override;
+    void
+    releaseResources() override;
 
 #ifndef JucePlugin_PreferredChannelConfigurations
-  bool isBusesLayoutSupported(const BusesLayout& layouts) const override;
+    bool
+    isBusesLayoutSupported(const BusesLayout &layouts) const override;
 #endif
 
-  void processBlock(AudioBuffer<float> &, MidiBuffer &) override;
+    void
+    processBlock(AudioBuffer<float> &, MidiBuffer &) override;
 
-  //==============================================================================
-  AudioProcessorEditor *createEditor() override;
-  bool hasEditor() const override;
+    //==============================================================================
+    AudioProcessorEditor *
+    createEditor() override;
+    bool
+    hasEditor() const override;
 
-  //==============================================================================
-  const String getName() const override;
+    //==============================================================================
+    const String
+    getName() const override;
 
-  bool acceptsMidi() const override;
-  bool producesMidi() const override;
-  bool isMidiEffect() const override;
-  double getTailLengthSeconds() const override;
+    bool
+    acceptsMidi() const override;
+    bool
+    producesMidi() const override;
+    bool
+    isMidiEffect() const override;
+    double
+    getTailLengthSeconds() const override;
 
-  //==============================================================================
-  int getNumPrograms() override;
-  int getCurrentProgram() override;
-  void setCurrentProgram(int index) override;
-  const String getProgramName(int index) override;
-  void changeProgramName(int index, const String &new_name) override;
+    //==============================================================================
+    int
+    getNumPrograms() override;
+    int
+    getCurrentProgram() override;
+    void
+    setCurrentProgram(int index) override;
+    const String
+    getProgramName(int index) override;
+    void
+    changeProgramName(int index, const String &new_name) override;
 
-  //==============================================================================
-  void getStateInformation(MemoryBlock &dest_data) override;
-  void setStateInformation(const void *data, int size_in_bytes) override;
+    //==============================================================================
+    void
+    getStateInformation(MemoryBlock &dest_data) override;
+    void
+    setStateInformation(const void *data, int size_in_bytes) override;
 
- private:
-  //==============================================================================
-  AudioProcessorValueTreeState apvts_;
+private:
+    //==============================================================================
+    AudioProcessorValueTreeState apvts_;
 
-  MidiProcessor midi_processor_{apvts_};
+    MidiProcessor midi_processor_{apvts_};
 
-  AudioProcessorValueTreeState::ParameterLayout createParameters() const;
+    AudioProcessorValueTreeState::ParameterLayout
+    createParameters() const;
 
-  JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(ToNegativeHarmonyProcessor)
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(ToNegativeHarmonyProcessor)
 };
